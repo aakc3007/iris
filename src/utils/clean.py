9f8db83d12b2b2452f397a221bced66c6ab0ce92
aakc3007 <aakc3007@gmail.com>
2025-06-23 09:14:15 +0100

    Added updates in clean.py

--- src/utils/clean.py
+++ src/utils/clean.py
@@ -1,1 +1,11 @@
 print('cleaning data')
+
+import pandas as pd
+
+
+def read_df(read_path):
+    df = pd.read_csv(read_path)
+    
+
+def save_df(df, save_path):
+    df.to_csv(save_path, index=False)
